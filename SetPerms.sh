6fb8e9593f16759219c9b0637aea50ff1378359b
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2014-09-05 10:36:51 -0700

    Create SetPerms.sh

---
+++ SetPerms.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd && chmod 755 ToolsSetup.sh && chmod 755 PathsSetup.sh && chmod 755 CMRepoSetup.sh && chmod 755 RepoSync.sh && chmod 755 BuildCM-jewel.sh && chmod 755 EnvironmentSetup.sh && chmod 755 SuperScript.sh && chmod 755 GetCommits.sh && chmod 755 Cleanup.sh
