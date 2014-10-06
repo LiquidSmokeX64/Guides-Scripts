7243fbf894637867b4f97fcc98bcdcc9f6c09359
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2014-10-06 10:12:32 -0700

    Update BuildCM-m7spr.sh

--- BuildCM-m7spr.sh
+++ BuildCM-m7spr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/android/system && ./vendor/cm/get-prebuilts && cd ~/android/system/ && source build/envsetup.sh && breakfast jewel && croot && brunch jewel
+cd ~/android/system && ./vendor/cm/get-prebuilts && cd ~/android/system/ && source build/envsetup.sh && breakfast m7spr && croot && brunch m7spr
