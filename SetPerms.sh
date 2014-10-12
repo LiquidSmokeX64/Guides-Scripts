3902a19b0367a6ec00bf562bd12e9f2915e75533
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2014-10-12 12:49:34 -0700

    Update SetPerms.sh

--- SetPerms.sh
+++ SetPerms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd && chmod 755 ToolsSetup.sh && chmod 755 PathsSetup.sh && chmod 755 UnityRepoSetup.sh && chmod 755 RepoSync.sh && chmod 755 BuildCM-jewel.sh && chmod 755 BuildCM-m7spr.sh && chmod 755 BuildCM-hammerhead.sh && chmod 755 EnvironmentSetup.sh && chmod 755 SuperScript.sh && chmod 755 GetCommits.sh && chmod 755 Cleanup.sh
+cd && chmod 755 ToolsSetup.sh && chmod 755 PathsSetup.sh && chmod 755 UnityRepoSetup.sh && chmod 755 RepoSync.sh && chmod 755 BuildCM-jewel.sh && chmod 755 BuildCM-m7spr.sh && chmod 755 BuildCM-hammerhead.sh && chmod 755 BuildCM-t0lte.sh && chmod 755 EnvironmentSetup.sh && chmod 755 SuperScript.sh && chmod 755 GetCommits.sh && chmod 755 Cleanup.sh
